3180be9662779974e5ffaab2a87ec0060775d769
sekarasriii <sekareeesh@gmail.com>
2024-05-27 11:28:20 +0700

    membuat class derivedclass

--- NoOverRiding/NoOverRiding.cpp
+++ NoOverRiding/NoOverRiding.cpp
@@ -11,11 +11,13 @@ public:
 
 
 class derivedClass : public baseClass {
+
 public:
 	void perkenalan() {
 		cout << "Hallo saya Function dari derived class";
 	}
 };
+
 
 int main() {
 	derivedClass a;
